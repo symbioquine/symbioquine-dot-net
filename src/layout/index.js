e52f8fa6b8a9f57922027b36b997201ec2d361e1
Symbioquine <symbioquine@gmail.com>
2021-12-16 07:31:10 -0800

    Fix favicon

--- src/layout/index.js
+++ src/layout/index.js
@@ -4,12 +4,14 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import config from '../../data/SiteConfig'
 import styles from './index.module.scss'
+import favicon from '../favicon.png'
 
 const MainLayout = ({ children }) => (
   <>
     <Header />
     <Helmet>
       <meta name="description" content={config.siteDescription} />
+      <link rel="icon" href={favicon} />
     </Helmet>
     {children}
     <Footer />
